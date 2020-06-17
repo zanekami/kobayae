43accc3d2718ceb064f7a82b902332d3270f74ec
kamizaneyae <kamizaneyae@192.168.3.13>
2020-06-17 21:04:58 +0900

    hello

---
+++ sw.kadai5/src/sw/kadai5/IntToEng.java
@@ -0,0 +1,22 @@
+package sw.kadai5;
+import java.util.Scanner;
+
+public class IntToEng {
+	// メインメソッド
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+        System.out.println(translateEng(input));
+    }
+
+    
+    
+    // 数値を英訳する変換するメソッド
+    static String translateEng(int n) {
+        return "";
+    }
+    
+}
+
+
+
