71129bb8056888da3668bcd9118dd0e9b2828a49
kobayashichihiro <kobayashichihiro@192.168.10.111>
2020-06-18 23:21:22 +0900

    ver4

--- sw.kadai5/src/sw/kadai5/IntToEng.java
+++ sw.kadai5/src/sw/kadai5/IntToEng.java
@@ -1,3 +1,4 @@
+//5桁目標
 package sw.kadai5;
 import java.util.Scanner;
 
@@ -27,36 +28,36 @@ public class IntToEng {
     		if(length==2&&c[0]!="1"||length<2||length>2) {
     			if(c[length-1]=="1") {
     				s[length-1]="one";
-				    				
-				 }else if(c[length-1]=="2") {
+			 　 }else if(c[length-1]=="2") {
 				    s[length-1]="two";
-				 }
 				}else if(c[length-1]=="3") {
 					s[length-1]="tree";
-				}
 				}else if(c[length-1]=="4") {
 					s[length-1]="four";
-			    }
-				}else if(c[length-1]=="5") {
+			    }else if(c[length-1]=="5") {
 					s[length-1]="five";
-				}
 				}else if(c[length-1]=="6") {
 					s[length-1]="six";
-				}
 				}else if(c[length-1]=="7") {
 					s[length-1]="seven";
-				}
 				}else if(c[length-1]=="8") {
 					s[length-1]="eight";
-				}
 				}else if(c[length-1]=="9") {
 					s[length-1]="nine";
-				}
 				}else if(c[length-1]=="2") {
 					s[length-1]="";
 				}
-				    	}
-    	}
+    		}
+    		if(length>1) {
+    			if(c[length-2]=='1') {
+    				s[length-2]="eleven";
+    			}else if(c[length-1]=='2') {
+    				s[length-2]="twelve";
+    			}
+    		}
+    			
+    		}
+    }
 }
 
 
--- sw.kadai5/src/sw/kadai5/TransTest.java
+++ sw.kadai5/src/sw/kadai5/TransTest.java
@@ -1,8 +1,22 @@
 
+
 public class TransTest {
 	@Test
 	public void test1桁() {
-		IntToEng t=new IntToEng();
+		IntToEng t = new IntToEng();
+		String expected = "one-digit";
+		String actual = t.translateEng(1);
+		
+		assertThat(actual,is(expected));
+	}
+	
+	@Test
+	public void zero() {
+		ntToEng t = new IntToEng();
+		String expected = "zero";
+		String actual = t.translateEng(0);
+		
+		assertThat(actual,is(expected));
 	}
 
 }
